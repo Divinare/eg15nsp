9cd96dc9af03591e940d02b92dfec48fc51a8c87
Miró Sorja <miro.sorja@digital-identity.com>
2015-05-24 14:34:18 +0300

    Added nav

--- backend-api/src/js/comp/Nav.js
+++ backend-api/src/js/comp/Nav.js
@@ -4,7 +4,31 @@ var Navigation = React.createClass({
 
   render: function() {
     return (
-      <h1>i am navi !!!</h1>
+      <div className="contain-to-grid sticky">
+        <nav className="top-bar" data-topbar role="navigation" data-options="sticky_on: large">
+{
+  // Title area
+}
+          <ul className="title-area">
+            <li className="name">
+              <h1><a href="#"><span className="fi-trees"/> EG15NSP </a></h1>
+            </li>
+            <li className="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+          </ul>
+
+
+            <section className="top-bar-section">
+              <ul className="right">
+                <li className="">
+                  <a href="#" className="">
+                    <span className="fi-social-squidoo"/> Fancy Button
+                  </a>
+                </li>
+              </ul>
+            </section>
+
+        </nav>
+      </div>
     );
   }
 
